8a87002530696b30c7246ed729c9d4dcada0c3e0
Heather <heather@live.ru>
2014-01-10 10:10:58 +0400

    can search on DuckDuckGo

--- Serene.rkt
+++ Serene.rkt
@@ -4,7 +4,7 @@
 (define f (new (class frame% (super-new)
         (define/augment (on-close)
           (displayln "Exiting...")))
-      [label "Search Serene 1.0"]
+      [label "Search Serene 1.1"]
       [min-width 350]))
 
 (define lmgfy (new text-field%
@@ -23,28 +23,49 @@
          [u (string-append g (uri-encode str))]
          [rx #rx"(?<=<h3 class=\"r\">).*?(?=</h3>)"])
     (regexp-match* rx (get-pure-port (string->url u)))))
+(define (let-me-duck-that-for-you str)
+  (let* ([g "https://duckduckgo.com/html/?q="]
+         [u (string-append g (uri-encode str))]
+         [rx #rx"(?<=<a rel=\"nofollow\" class=\"large\").*?(?=</a)"])
+    (regexp-match* rx (get-pure-port (string->url u)))))
+
 (define (postparse strs)
-  
   (for ([ch (send group-box-panel get-children)])
     (send group-box-panel delete-child ch))
-  
   (for ([str strs])
     (define zp (new horizontal-panel%
                [parent group-box-panel]
                [alignment '(left center)]))
-    
-    ;(displayln str) #| DEBUG |#
     (define link
       (regexp-replace* #px"url[?]q=" 
-      (bytes->string/utf-8 (car (let ([rx #rx"(?<= href=\"/).*?(?=\">)"])
+      (bytes->string/utf-8 (car (let ([rx #rx"(?<= href=\"/).*?(?=\" ?>)"])
                          (regexp-match rx str)))) ""))
     (define lbl
       (regexp-replace* #px"</?b>" 
         (bytes->string/utf-8 (car (let ([rx #rx"(?<=\">).*?(?=</a>)"])
                          (regexp-match rx str)))) ""))
     (make-object button% "Open" zp (λ (btn evt)
-             (send-url link)
-           #|(message-box "link" link)|#)) #| DEBUG |#
+             (send-url link)))
+    (make-object message% lbl zp)))
+
+(define (postparse-duck strs)
+  (for ([ch (send group-box-panel get-children)])
+    (send group-box-panel delete-child ch))
+  (for ([str strs]
+        [i (in-naturals 1)]
+        #:when (< i 11))
+    (define zp (new horizontal-panel%
+               [parent group-box-panel]
+               [alignment '(left center)]))
+    (define link
+      (bytes->string/utf-8 (car (let ([rx #rx"(?<=href=\").*?(?=\">)"])
+                         (regexp-match rx str)))))
+    (define lbl
+      (regexp-replace* #px"</?b>" 
+        (bytes->string/utf-8 (car (let ([rx #rx"(?<=\">).*"])
+                         (regexp-match rx str)))) ""))
+    (make-object button% "Open" zp (λ (btn evt)
+             (send-url link) #|(message-box "link" link)|# )) #| DEBUG |#
     (make-object message% lbl zp)))
 
 (define p (new horizontal-panel%
@@ -55,11 +76,16 @@
                     [label "Google"]
                     [callback (λ (btn evt)
      (postparse (let-me-google-that-for-you (send lmgfy get-value))))]))
+(define duck (new button%
+                  [parent p]
+                  [label "DuckDuckGo"]
+                  [callback (λ (btn evt)
+     (postparse-duck (let-me-duck-that-for-you (send lmgfy get-value))))]))
 (define about (new button%
                     [parent p]
                     [label "About"]
                     [callback (λ (btn evt)
-     (message-box "Search Serene" "by Heather"))]))
+     (message-box "Search Serene" "  Search Serene 1.1 by Heather  "))]))
 
 (send f show #t)
 
--- readme.md
+++ readme.md
@@ -3,23 +3,26 @@ Search Serene
 
 Simple stupid search application; my first scheme (racket) experiment based on examples >_<
 
+Currently
+=========
+
+ - Can search on google.com
+ - Can search on duckduckgo
+
 ``` racket
 (define (let-me-google-that-for-you str)
   (let* ([g "https://www.google.com/search?q="]
          [u (string-append g (uri-encode str))]
          [rx #rx"(?<=<h3 class=\"r\">).*?(?=</h3>)"])
     (regexp-match* rx (get-pure-port (string->url u)))))
+
 (define (postparse strs)
-  
   (for ([ch (send group-box-panel get-children)])
     (send group-box-panel delete-child ch))
-  
   (for ([str strs])
     (define zp (new horizontal-panel%
                [parent group-box-panel]
                [alignment '(left center)]))
-    
-    ;(displayln str) #| DEBUG |#
     (define link
       (regexp-replace* #px"url[?]q=" 
       (bytes->string/utf-8 (car (let ([rx #rx"(?<= href=\"/).*?(?=\">)"])
@@ -29,7 +32,6 @@ Simple stupid search application; my first scheme (racket) experiment based on e
         (bytes->string/utf-8 (car (let ([rx #rx"(?<=\">).*?(?=</a>)"])
                          (regexp-match rx str)))) ""))
     (make-object button% "Open" zp (? (btn evt)
-             (send-url link)
-           #|(message-box "link" link)|#)) #| DEBUG |#
+             (send-url link) #|(message-box "link" link)|# ))
     (make-object message% lbl zp)))
 ```
