c6782e2f94f3b9ebeb2c9abacf01057a6a6c1726
Heather <heather@live.ru>
2014-01-24 11:02:19 +0400

    lesser

--- ss.rkt
+++ ss.rkt
@@ -5,32 +5,37 @@
   (display-to-file "" ".ss"
                    #:mode 'text
                    #:exists 'truncate/replace))
-(define (save v)
-  (display-to-file (string-append v "\n") ".ss"
-                   #:mode 'text
-                   #:exists 'append))
+(define (save v p)
+  (if (= p 0)
+      (display-to-file (string-append v "\n") ".ss"
+                       #:mode 'text
+                       #:exists 'append)
+      (display-to-file (string-append v "\n") ".ss"
+                       #:mode 'text
+                       #:exists 'append)))
 (define (open v)
   (send-url
    (list-ref (file->lines ".ss" #:mode 'text)
              (- v 1))))
 
-(define (serene base baserx i0 i1 getHttp getDescription)
+(define (serene base baserx i0 getHttp getDescription)
   (λ (target)
-    (for ([str (let ([u (string-append base (uri-encode target))])
-                 (regexp-match* baserx (get-pure-port (string->url u))))]
+    (let ([u (string-append base (uri-encode target))])
+    (for ([str (regexp-match* baserx (get-pure-port (string->url u)))]
           [i (in-naturals 1)]
-          #:when (and (< i i1) (> i i0)))
-      (display (- i i0))
-      (display " : ")
-      (let ([http (getHttp str)])
-        (display http)
-        (save http))
-      (display " > ")
-      (displayln  (getDescription str)))))
+          #:when (> i i0))
+      (let* ([http (getHttp str)]
+             [desc (getDescription str)])
+              (display (- i i0))
+              (display " : ")
+              (display http)
+              (save http 0)
+              (display " > ")
+              (displayln desc)
+        )))))
 
 (let* ([duck-search (serene "https://duckduckgo.com/html/?q="
-                            #rx"(?<=<a rel=\"nofollow\" class=\"large\").*?(?=</a>)"
-                            1 11
+                            #rx"(?<=<a rel=\"nofollow\" class=\"large\").*?(?=</a>)" 1
                             (λ (str) (bytes->string/utf-8 
                                       (car (let ([rx #rx"(?<=href=\").*?(?=\">)"])
                                              (regexp-match rx str)))))
@@ -39,8 +44,7 @@
                                                                                   (regexp-match rx str))))"")))]
        
        [google-search (serene "https://www.google.com/search?q="
-                              #rx"(?<=<h3 class=\"r\">).*?(?=</h3>)"
-                              0 11
+                              #rx"(?<=<h3 class=\"r\">).*?(?=</h3>)" 0
                               (λ (str) (regexp-replace* #px"url[?]q=" 
                                                         (bytes->string/utf-8 (car (let ([rx #rx"(?<= href=\"/).*?(?=&amp)"])
                                                                                     (regexp-match rx str)))) ""))
@@ -55,14 +59,14 @@
   (command-line
    #:once-each
    [("-v" "--version") "Display search serene version"
-                       (set! no-v? #false)]
+                    (set! no-v? #false)]
    [("-o" "--open") string "Open an link"
                     (set! openlink string)]
    #:once-any
    [("-d" "--duck") "Duck Duck Go"
                     (set! google-mode? #false)]
    [("-g" "--google") "Google"
-                      (set! google-mode? #true)]
+                    (set! google-mode? #true)]
    #:args all (if no-v?
                   (if (string=? openlink "")
                       (let ([A (string-join all)]) (wipe)
